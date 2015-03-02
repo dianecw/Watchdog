c56f8bfbeec2b7be7cd19e83d52eedd3ba8734ae
Ethan Chiou <ethanchiou@berkeley.edu>
2015-03-01 20:15:16 -0800

    ghetto af

--- watchdog.py
+++ watchdog.py
@@ -1,16 +1,40 @@
 import RPi.GPIO as GPIO
+import time
 
+
+#initialize constants
+wait_time = 1
+led1 = 4
+led2 = 17
+led3 = 22
+led4 = 10
+
+#set up pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(4, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(17, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(22, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setwarnings(False)
+GPIO.setup(led1, GPIO.OUT)
+GPIO.setup(led2, GPIO.OUT)
+GPIO.setup(led3, GPIO.OUT)
+GPIO.setup(led4, GPIO.OUT)
 
-while True:
+#turn off all leds
+GPIO.output(led1, 0)
+GPIO.output(led2, 0)
+GPIO.output(led3, 0)
+GPIO.output(led4, 0)
 
-	GPIO.output(2) = 1
-	GPIO.output(4) = 1
-	GPIO.output(17) = 1
-	GPIO.output(22) = 1
+#alternate leds sequentially
+GPIO.output(led1, 1)
+time.sleep(1)
+GPIO.output(led1, 0)
+GPIO.output(led2, 1)
+time.sleep(1)
+GPIO.output(led2, 0)
+GPIO.output(led3, 1)
+time.sleep(1)
+GPIO.output(led3, 0)
+GPIO.output(led4, 1)
+time.sleep(1)
+GPIO.output(led4, 0)
 
 GPIO.cleanup()
